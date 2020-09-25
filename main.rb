a5c65ddc5d59889ea5a8a6f8504494d32904f483
Matthew Williams <mattwilly98@hotmail.com>
2020-09-25 13:16:37 +1000

    added tty-pompt and logic for login menu

--- main.rb
+++ main.rb
@@ -1,4 +1,34 @@
 require 'csv'
-require_relative '/classes/admin'
-require_relative '/classes/public'
+require 'tty-prompt'
+# require_relative '/classes/admin'
+# require_relative '/classes/public'
+require_relative 'classes/user'
+
+
+prompt = TTY::Prompt.new
+account_choice = 'Login or Create'
+choices = %w(Login Create_account)
+answer = prompt.select(account_choice, choices)
+if answer == choices[0]
+    puts "Username:"
+    username = gets.chomp
+    puts "Password:"
+    password = gets.chomp
+    data = CSV.parse(File.read("users.csv"), headers: true)
+    data.each do |row|
+        if username == row["username"] && password == row["password"]
+            puts "#{row["username"]}: Logged in."
+        end
+    end
+elsif answer == choices[1]
+    puts "Username:"
+    username = gets.chomp
+    puts "Password:"
+    password = gets.chomp
+    CSV.open("user.csv", "wb", headers: true) do |csv|
+    csv << [username, password, false]
+    end
+end
+
+
 
