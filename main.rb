59330f73e4b919a96752b59396421f813a83fea1
Matthew Williams <mattwilly98@hotmail.com>
2020-09-26 15:40:00 +1000

    Withdraw feature working and tested

--- main.rb
+++ main.rb
@@ -51,11 +51,11 @@ if answer == choices[1]
         end
         if new_user == true
             CSV.open("users.csv", "a") do |csv|
-                csv << [@username,password,false]
+                csv << [@username,password,0,false]
                 csv.close
             end
             puts "Account created: hello #{@username}"
-            sleep 1
+            sleep 1.5
             break
         end
     end
@@ -79,19 +79,51 @@ loop do
                 if balance_selction == balance_options[2]
                     break
                 elsif balance_selction == balance_options[0]
+                    # deposit section
                     puts "How much do you want to deposit?"
                     deposit = gets.chomp.to_i
                     if deposit > 0
-                       puts "i should deposit money"
-                       sleep 1.5
-                    elsif deposit <= 0
+                        new_balance = row["balance"].to_i + deposit
+                        row["balance"] = new_balance.to_s
+                        puts "you now have: $#{new_balance}"
+                        File.write("users.csv", data) do |row|
+                            row["balance"] << [new_balance]
+                        end
+                        sleep 1
+                    elsif deposit == 0
                         puts "You must enter a number above 0"
                         sleep 1.5
                     end
+                elsif balance_selction == balance_options[1]
+                    #withdraw section
+                    loop do
+                        system "clear"
+                        puts "How much do you want to withdraw?"
+                        withdraw = gets.chomp.to_i
+                        new_balance = row["balance"].to_i - withdraw
+                        if withdraw <= 0
+                            puts "Enter a number greater then 0"
+                            sleep 1.3
+                        elsif new_balance < 0
+                            puts "Can't withdraw more then you have!"
+                            sleep 1.3
+                        elsif new_balance >= 0
+                            row["balance"] = new_balance.to_s
+                            puts "you now have: $#{new_balance}"
+                            File.write("users.csv", data) do |row|
+                                row["balance"] << [new_balance]
+                            end
+                            sleep 1
+                            break
+                        end
+                    end
                 end
-
             end
         end
+    elsif menu_selction == menu_options[1]
+        #make a bet
+    elsif menu_selction == menu_options[2]
+        #bet history
     elsif menu_selction == menu_options[3]
         system "clear"
         puts "logging out"
